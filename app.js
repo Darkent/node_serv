baf1821187fb7af85a5167146592bd3e9e8bd081
Darkent <Darkent4ever@gmail.com>
2022-06-01 10:25:06 -0500

    fx2

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/", require("./routes"));
 app.use("/files", express.static("uploads"));
-app.listen(3000, () => console.log("Corriendo..."));
+app.listen(process.env.PORT || 3000, () => console.log("Corriendo..."));
