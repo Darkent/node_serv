34c96b5c7ab6961d0bccc6d5ed9b4d8122331286
Darkent <Darkent4ever@gmail.com>
2022-06-01 11:05:29 -0500

    f3

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", require("./routes"));
 app.use("/files", express.static("uploads"));
-app.listen(process.env.PORT || 3000, () => console.log("Corriendo..."));
+app.listen(process.env.PORT || 3000, () => {
+  let p = `./files/products.txt`;
+
+  console.log(path.resolve(p));
+
+  console.log("Corriendo...");
+});
